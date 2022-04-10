5c83abf4b0c592644881cce21cfcba454c6f3fec
Harmohit Singh <harmohitsingh05@gmail.com>
2022-04-09 18:58:32 -0700

    Anjali updated code

--- VisiteFacile/app/src/main/java/com/example/visitefacile/DetailsActivity.java
+++ VisiteFacile/app/src/main/java/com/example/visitefacile/DetailsActivity.java
@@ -34,6 +34,7 @@ public class DetailsActivity extends AppCompatActivity {
         TextView myPrice = findViewById(R.id.txtViewPrice);
         ImageView myBackImage = findViewById(R.id.imageView3);
         ImageView imageViewHeart = findViewById(R.id.imgViewFav);
+        TextView myAbout = findViewById(R.id.textView10);
 
 
 
@@ -78,6 +79,7 @@ public class DetailsActivity extends AppCompatActivity {
                 myDestImage.setImageResource(myDest.getDestinationPic());
                 myCityName.setText(myDest.getDestinationName());
                 myPrice.setText(Double.toString(myDest.getDestinationTicketPrice()));
+                myAbout.setText(myDest.getDestinationAbout());
             }
         });
 
